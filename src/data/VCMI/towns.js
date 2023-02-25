1141f991db279439c3be533b2278ad60251ca002
krs <cristianlaczko@yahoo.com>
2023-02-25 18:35:03 +0200

    Initial upload. Forge2k faction configs and images

---
+++ src/data/VCMI/towns.js
@@ -0,0 +1,15 @@
+import FORGE2k_UNITS from './units/forge2k';
+import NEUTRAL_UNITS from './units/VCMI_neutral';
+
+export default {
+	FORGE2k: {
+		img: 'forge2k.gif',
+		slug: 'forge2k',
+		units: FORGE2k_UNITS,
+	},
+	VCMI_NEUTRAL: {
+		img: 'neutral.gif',
+		slug: 'vcmi_neutral',
+		units: VCMI_NEUTRAL_UNITS,
+	},
+};
